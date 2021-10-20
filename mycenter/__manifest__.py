638f40fffe2d389c665e706aa6f26e86f4b9330f
thanhloiht <69473168+thanhloiht@users.noreply.github.com>
2021-10-20 15:13:23 +0700

    Thanh push code
    Tạo module mycenter, model customers

---
+++ mycenter/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Central Management",
+    'summary': """Central Management Software""",
+    'description': """Central Management Software""",
+    'author': "group6.info",
+    'website': "https://group6.info",
+    'category': 'Productivity',
+    'version': '1.0',
+    'depends': [],
+    'data': [
+        ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
