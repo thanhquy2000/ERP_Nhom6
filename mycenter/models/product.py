318f07307929b4282af860ca1399f43ceec3d53a
thanhquy2000 <69490802+thanhquy2000@users.noreply.github.com>
2021-10-26 18:15:12 +0700

    quy update

--- mycenter/models/product.py
+++ mycenter/models/product.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields,models,api
+from odoo import fields, models, api
+
 
 class ProductTemplateInherit(models.Model):
     _name = 'product.template'
     _inherit = 'product.template'
-    
-    date_test = fields.Datetime(string="Test Date",required=True)
-    name_teacher = fields.Char(string ='Teacher Name', required = True)
+
+    date_test = fields.Datetime(string="Test Date", required=True)
+    name_teacher = fields.Char(string='Teacher Name', required=True)
--- mycenter/models/sale.py
+++ mycenter/models/sale.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models
 
+
 class SaleExam(models.Model):
     _inherit = "sale.order"
     state = fields.Selection(selection_add=[('disable', 'Disabled')],
-    string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
-    PhoneNumber =fields.Char(string='Phone Number', required=False)
-    
+                             string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
+    PhoneNumber = fields.Char(string='Phone Number', required=False)
+
     def action_disable(self):
         self.state = 'disable'
 
