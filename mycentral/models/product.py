19a166f52684624c8d0c4d865c26583b1512580f
Lê Kiều Linh <181121521115@due.udn.vn>
2021-10-31 20:41:23 +0700

    Linh up

---
+++ mycentral/models/product.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields,models,api
+
+class ProductTemplateInherit(models.Model):
+    _name = 'product.template'
+    _inherit = 'product.template'
+    
+    date_test = fields.Datetime(string="Test Date",required=True)
+    name_teacher = fields.Char(string ='Teacher Name', required = True)
